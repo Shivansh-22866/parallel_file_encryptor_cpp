d099c13610013a6812efe3f74e88aebe8e65c1a7
Shivansh-22866 <shivanshp0418@gmail.com>
2024-08-30 00:45:47 +0530

    Implementing Cryption algo

---
+++ src/app/encryptDecrypt/CryptionMain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Cryption.hpp"
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cerr << "Usage:./caesar_cipher <text>" << std::endl;
+        return 1;
+    }
+
+    executeCryption(argv[1]);
+    return 0;
+}
